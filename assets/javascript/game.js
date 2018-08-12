9bf3acab3a6a1095e297fc5dbd13044403ed5cca
bryanlee <blee@utahsbr.edu>
2018-08-12 15:13:57 -0600

    made several updates to randomize images and values after eah round

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,67 +1,110 @@
 //begin game program code
 
 $(document).ready(function() {
+
     //array for crystal images, variables for current total, wins, losses
-    var valueOptions = [3, 5, 7, 10];
-    var crystals = ["./assets/images/ruby.png", "./assets/images/sapphire.png", "./assets/images/swarovski.png", "./assets/images/topaz.png"];
+    var valueOptions = [ 3, 5, 7, 10 ];
+    var crystals = 
+        [ "./assets/images/ruby.png",
+        "./assets/images/sapphire.png",
+        "./assets/images/swarovski.png",
+        "./assets/images/topaz.png" ];
     var currentTotal = 0;
     var wins = 0;
     var losses = 0;
-    //shuffle valueOptions
+
+    //function to shuffle array
     function shuffle (o) {
-        for (var j, x, i = o.length; i; j = parseInt(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+        for(let i = 0; i < o.length * 2; i++) {
+          // Get a random index into o, modding to stay withing array length.
+          const index = parseInt(Math.random() * o.length * 10, 10) % o.length;
+          // Pull that random value out and stick it on the front of the array.
+          o.unshift(o.splice(index, 1)[0])
+        }
         return o;
-    }
+      }
+
+    //testing shuffle
+    console.log(valueOptions);
+    console.log(shuffle(valueOptions));
+
+    //function to shuffle both value options and crystal order
+    function shuffleArrays() {
+        shuffle(valueOptions);
+        shuffle(crystals);
+    };
+
     //function to generate the random number the user will try to match
-    var numGen = function () {
+    function numGen () {
         targetNumber = Math.floor(20 + Math.random() * 60);
-        console.log(targetNumber);
         $("#randomNum").text(targetNumber);
         currentTotal = 0;
         $("#currentTotal").text(currentTotal);
-    }
+    };
+
     //run intial numGen function
     numGen(); 
-    //shuffle value options and crystal order
-    shuffle(valueOptions);
-    shuffle(crystals);
-    //set variables to the appropriate areas in the html
-    for (var i = 0; i < valueOptions.length; i++) {
-        //for each value in the valueOptions array, create an img tag for a crystal
-        var crystalImage = $("<img>");
-        //add style class to all img tags
-        crystalImage.addClass("crystal-image");
-        //each imageCrystal will be given a src link to the crystal image from the 
-        crystalImage.attr("src", crystals[i]);
-        //each crystalImage will be given a data attribute called data-crystalValue.
-        //this data attribute will be set equal to the array value.
-        crystalImage.attr("data-crystalvalue", valueOptions[i]);
-        //add each crystal into the #crystals area at the end of the last
-        $("#crystals").append(crystalImage);
-    }
-    //on click function to add value to currentTotal
+
+    //function to create crystal options images with values and src tags each round
+    function crystalGen() {
+
+        //make sure content area is cleared of any previous content
+        $("#crystals").empty();
+        
+        //run initial shuffle of arrays
+        shuffleArrays();
+
+        for (var z = 0; z < valueOptions.length; z++) {
+
+            //for each value in the valueOptions array, create an img tag for a crystal
+            var crystalImage = $("<img class='crystal-image'>");
+
+            crystalImage.attr("data-crystalValue", 0);
+
+
+            //each imageCrystal will be given a src link to the crystal image from the array
+            crystalImage.attr("src", crystals[z]);
+
+            //each crystalImage will be given a data attribute called data-crystalValue.
+            //this data attribute will be set equal to the array value.
+            crystalImage.attr("data-crystalValue", valueOptions[z]);
+
+            //add each crystal into the #crystals area at the end of the last
+            $("#crystals").append(crystalImage);
+
+        }
+    };
+
+    //run initial crystal gen to display crystals assign values
+    crystalGen();
+
+    //on click function to add value to current total as gems are clicked
     $(".crystal-image").on("click", function() {
+
         //define value for crystal value
-        var crystalValue = ($(this).attr("data-crystalvalue"));
+        var crystalValue = ($(this).attr("data-crystalValue"));
         crystalValue = parseInt(crystalValue);
+
         //add clicked crystal value to current total
         currentTotal += crystalValue;
+
         //update total in DOM
         $("#currentTotal").text(currentTotal);
-        //if, else if for game win/lose logic
+
+        //if, else if logic for game win/lose scenarios
+
         //if your current total matches the target number exactly, they win the round
         if (currentTotal === targetNumber) {
-            wins++;
             //add to win count
+            wins++;
+            //reflect in wins counter window
             $("#winsCount").text(wins);
-            //alert the player they have won
+            //alert the player they have won the round
             alert("You've won this round!")
-            //reshuffle value options
-            shuffle(valueOptions);
-            //reshuffle crystals
-            shuffle(crystals);
             //regenerate target number
             numGen();
+            crystalGen();
+
         //if the player's current total exceeds the target number, they lose the round
         } else if (currentTotal > targetNumber) {
             //add to losses count
@@ -69,17 +112,17 @@ $(document).ready(function() {
             $("#lossesCount").text(losses);
             //alert the player they have lost
             alert("You've lost this round!");
-            //reshuffle value options
-            shuffle(valueOptions);
-            //reshuffle crystals
-            shuffle(crystals);
             //regenerate target number
             numGen();
-        }
+            crystalGen();
+        } 
+
     });
+
     //click function for hiding instructions
     var instructions = $("#instructions");
     $(instructions).on("click", function () {
         $("p").slideToggle(1000);
-    })
+    });
+
 });
